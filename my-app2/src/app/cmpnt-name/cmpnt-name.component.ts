7df14429d377c7ec771c05ea68496450da88149e
fuadsami <fuadsami5@gmail.com>
2017-03-21 15:38:10 +0200

    ex2

---
+++ my-app2/src/app/cmpnt-name/cmpnt-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cmpnt-name',
+  templateUrl: './cmpnt-name.component.html',
+  styleUrls: ['./cmpnt-name.component.css']
+})
+export class CmpntNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
